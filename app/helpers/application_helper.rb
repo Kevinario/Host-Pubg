3895d57409dd8aada8833b30593ca85d647b94f5
WMaho <WilliamMahon99@hotmail.com>
2017-11-27 12:50:58 +0000

    Added Purchases and modified login layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    
+    def loggedState
+        if user_signed_in?
+            return [(link_to "Logout", destroy_user_session_path, method: :delete)]
+        else
+            return [(link_to "Login", new_user_session_path),(link_to "Signup", new_user_registration_path)]
+        end
+    end
 end
